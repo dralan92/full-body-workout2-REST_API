256af9d00dd4afe379785b9e39a42e618a654b75
unknown <codename100.alan@gmail.com>
2018-08-30 03:06:11 -0400

    Error Handling Done

--- app.js
+++ app.js
@@ -6,4 +6,18 @@ const chestRoutes = require('./api/routes/chest');
 app.use(morgan('dev'));
 app.use('/chest',chestRoutes );
 
+app.use((req,res,next)=>{
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req,res,next)=>{
+    res.status(error.status||500).json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
 module.exports = app; 
